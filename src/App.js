fbe63eb87af9a72c0114bdf55fc9eaf11fed631e
Ivan Lagunski <54036470+Lagunskij@users.noreply.github.com>
2022-03-09 10:26:31 +0300

    3 commit rout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-
 import './App.css';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import {Main} from "./components/Main";
+import {Card} from "./components/card/card";
 
 function App() {
-  return (
-      <BrowserRouter>
-        <div className="container">
-<Route path="/" component={Main}/>
-        </div>
-      </BrowserRouter>
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Switch>
+                    <Route exact path="/" component={Main}/>
+                    <Route path="/card" component={Card}/>
+                    <Redirect to={"/"} />
+                </Switch>
 
-  );
+            </div>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
--- src/components/repo/Repo.jsx
+++ src/components/repo/Repo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './repo.css'
+import {NavLink} from "react-router-dom";
 
 export const Repo = (props) => {
     const repo = props.repo
@@ -8,11 +9,11 @@ export const Repo = (props) => {
     return (
         <div className={'repo'}>
             <div className="repo-header">
-                <div className="repo-header-name">{repo.name}</div>
+                <div className="repo-header-name"> <NavLink to={'/card'}>{repo.name}</NavLink> </div>
                 <div className="repo-header-stars">{repo.stargazers_count}</div>
             </div>
             <div className="repo-last-commit">Последний комит:{repo.updated_at}</div>
             <a href={repo.html_url} className={"repo-link"}>Ссылка: {repo.html_url}</a>
         </div>
-    )
-}
+    );
+};
